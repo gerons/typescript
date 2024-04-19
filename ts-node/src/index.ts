463967d009c1b7d8d675531e229c3bdae6f3822d
Gero Nñz <gerons69@gmail.com>
2024-04-19 17:41:17 -0600

    ejercicios generics termiandos

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,23 +1,7 @@
-import { printObjet, genericFunction, genericFunctionArrow } from "./generics/generics";
-import { Villain, Hero } from "./interfaces";
+import { getPokemon } from "./generics/get-pokemon";
 
 
-// printObjet(123)
-// printObjet( new Date())
-
-// console.log(genericFunction(3.14163455).toFixed(2))
-// console.log(genericFunction('Hola Mundo').toLocaleLowerCase())
-// console.log(genericFunction(new Date()).getDate())
-
-// console.log(genericFunctionArrow(3.14163455).toFixed(2))
-// console.log(genericFunctionArrow('Hola Mundo').toLocaleLowerCase())
-// console.log(genericFunctionArrow(new Date()).getDate())
-
-
-const deadpool = {
-    name: 'Deadpool',
-    realName: 'Wade Winston Wilson',
-    dangerLevel: 130
-}
-
-console.log(genericFunctionArrow<Villain>(deadpool))
+getPokemon(1)
+    .then( pokemon => console.log( pokemon.name.toUpperCase() ) )
+    .catch( error => console.log( error ) )
+    .finally( () => console.log('Finalizado...') )
--- ts-node/src/interfaces/index.ts
+++ ts-node/src/interfaces/index.ts
@@ -1,3 +1,3 @@
 export { Hero } from "./hero";
 export { Villain } from './villain';
-
+import { Pokemon } from './pokemon';
